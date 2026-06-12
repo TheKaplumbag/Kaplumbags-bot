89aeecb52056244c91003daae4c186989bd02da5
TheKaplumbag <scpmtf4@gmail.com>
2026-06-13 02:29:36 +0300

    Update Kaplumbags-bot from Acode

---
+++ cogs/Core.py
@@ -0,0 +1,18 @@
+import discord
+import logging
+from discord import app_commands
+from discord.ext import commands
+
+class Core(commands.Cog):
+  def __init__(self, bot: commands.Bot):
+    self.bot = bot
+  
+  @app_commands.command(name="ban", description="Test")
+  async def ping(self, 
+  interaction:discord.Interaction, 
+  user: discord.User,
+  reason: str):
+    await interaction.response.send_message(f"{user} has been banned! for {reason}")
+    
+async def setup(bot: commands.Bot):
+  await bot.add_cog(Core(bot))
