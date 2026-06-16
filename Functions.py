eca3c56cb53b35c1e22c557dd72b58fbb29ca3d4
TheKaplumbag <scpmtf4@gmail.com>
2026-06-16 17:23:07 +0300

    Quick commit: Update Functions.py

--- Functions.py
+++ Functions.py
@@ -66,8 +66,8 @@ def UserGameBan(userId: int, duration: str, display_reason: str, private_reason:
         print(f"An error occurred: {e}")
         return False
   
-
-def GetFullBanList():
+  
+def GetGameBanHistory():
   url = f"https://apis.roblox.com/cloud/v2/universes/{UNIVERSE_ID}/user-restrictions:listLogs"
   headers = {
     "x-api-key": API_KEY,
@@ -107,7 +107,6 @@ def GetFullBanList():
     return f"⚠️ API Error: {response.status_code}"
 
 
-
 def UnGameBan(userId: int, display_reason: str, private_reason: str):
   url = f"https://apis.roblox.com/cloud/v2/universes/{UNIVERSE_ID}/user-restrictions/{userId}"
   headers={
@@ -130,5 +129,44 @@ def UnGameBan(userId: int, display_reason: str, private_reason: str):
   except requests.exceptions.RequestException as e:
     print(f"An error occurred: {e}")
     return False
+
+
+def GetPlayerHistory(userId: int, player: str):
+  url = f"https://apis.roblox.com/cloud/v2/universes/{UNIVERSE_ID}/user-restrictions:listLogs"
+  headers={
+    "x-api-key": API_KEY,
+    "Content-Type": "application/json"
+  }
+  query={
+    "filter": f"user == 'users/{userId}'"
+  }
+  response = requests.get(url, headers=headers, params= query)
   
-  
+  if response.status_code == 200:
+    data = json.loads(response.text)
+    
+    if "logs" not in data or not data["logs"]:
+      return "📋 **Ban Logs:** No active bans found."
+    
+    formatted_message = f"📋 **ROBLOX BAN LOG OF {player.strip()}**\n\n"
+    
+    for log in data["logs"]:
+      user_id = log["user"].split("/")[-1]
+      mod_id = log["moderator"]["robloxUser"].split("/")[-1]
+      
+      user_link = f"https://www.roblox.com/users/{user_id}/profile"
+      mod_link = f"https://www.roblox.com/users/{mod_id}/profile"
+      
+      public_reason = log.get("displayReason", "No reason provided")
+      private_reason = log.get("privateReason", "No internal reason")
+      
+      formatted_message += f"👤 **User:** [Profile Link]({user_link}) *(ID: {user_id})*\n"
+      formatted_message += f"🛠️ **Moderator:** [Profile Link]({mod_link})\n"
+      formatted_message += f"📄 **Reason:** {public_reason}\n"
+      formatted_message += f"🔒 **Internal Note:** *{private_reason}*\n"
+      formatted_message += "---------------------------------------\n"
+        
+    return formatted_message
+  else:
+    print(f"API Error {response.status_code}: {response.text}")
+    return f"⚠️ API Error: {response.status_code}"
